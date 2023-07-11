3d1827ee251b3f050f30e9e2ad39a5aec0c25d75
FlyingDutchman2021 <89907122+FlyingDutchman2021@users.noreply.github.com>
2023-07-12 03:36:54 +0800

    Finish choose: teacher feacher added

--- ChooseDetailView.py
+++ ChooseDetailView.py
@@ -32,20 +32,17 @@ class ChooseDetailView:
         self.add_t_id = tk.StringVar()
         self.add_chosen_year = tk.StringVar()
 
-
-
-
-
-
+        # 4
+        self.teacher_mod_search_s_id = tk.StringVar()
+        self.teacher_mod_search_c_id = tk.StringVar()
+        self.teacher_mod_search_t_id = tk.StringVar()
+        self.tm_mod_score = tk.StringVar()
+        self.tm_current_selected_s_id = '---'
+        self.tm_current_selected_c_id = '---'
+        self.tm_current_selected_t_id = '---'
 
         # Create Display Frame
         self.main_frame = tk.Frame(window)
-
-
-
-
-
-
 
         # Create Table Frame
         self.tree_frame = tk.Frame(self.main_frame)
@@ -78,14 +75,6 @@ class ChooseDetailView:
 
         self.scrollbar.pack(side='right', fill='y', pady=15)
         self.tree.pack(side='left', padx=5, pady=15)
-
-
-
-
-
-
-
-
 
         # Create Entry Frame
         self.entry_frame = tk.Frame(self.main_frame)
@@ -134,15 +123,6 @@ class ChooseDetailView:
 
         # 3
         self.button_search.grid(row=1, column=6)
-
-
-
-
-
-
-
-
-
 
         # Create Modification Frame
         self.mod_frame = tk.Frame(self.main_frame)
@@ -207,14 +187,66 @@ class ChooseDetailView:
         # 5
         self.label_update_succeed_status.grid(row=2, column=5)
 
+        # Create Teacher Mod Frame
+        self.teacher_mod_frame = tk.Frame(self.main_frame)
+        # 1
+        self.label_teacher_mod_search_s_id_hint = tk.Label(self.teacher_mod_frame, text='Student ID',
+                                                           font=config.LABEL_FONT)
+        self.entry_teacher_mod_search_s_id = tk.Entry(self.teacher_mod_frame, textvariable=self.teacher_mod_search_s_id,
+                                                      font=config.ENTRY_FONT, width=config.S_ENTRY_WIDTH[0])
+        self.label_teacher_mod_search_c_id_hint = tk.Label(self.teacher_mod_frame, text='Course ID',
+                                                           font=config.LABEL_FONT)
+        self.entry_teacher_mod_search_c_id = tk.Entry(self.teacher_mod_frame, textvariable=self.teacher_mod_search_c_id,
+                                                      font=config.ENTRY_FONT, width=config.S_ENTRY_WIDTH[2])
+        self.label_teacher_mod_search_t_id_hint = tk.Label(self.teacher_mod_frame, text='Teacher ID',
+                                                           font=config.LABEL_FONT)
+        self.entry_teacher_mod_search_t_id = tk.Entry(self.teacher_mod_frame, textvariable=self.teacher_mod_search_t_id,
+                                                      font=config.ENTRY_FONT, width=config.S_ENTRY_WIDTH[4])
 
+        # 2
+        self.button_tm_id_search = tk.Button(self.teacher_mod_frame, text='Search', font=config.BUTTON_MEDIUM,
+                                             command=lambda: self.tm_id_search(), pady=2, padx=10)
+        self.button_tm_update = tk.Button(self.teacher_mod_frame, text='Update', font=config.BUTTON_MEDIUM,
+                                          command=lambda: self.tm_update(), pady=2, padx=10)
+        # 3
+        self.label_teacher_mod_search_s_id = tk.Label(self.teacher_mod_frame, text='---', font=config.ENTRY_FONT)
+        self.label_teacher_mod_search_c_id = tk.Label(self.teacher_mod_frame, text='---', font=config.ENTRY_FONT)
+        self.label_teacher_mod_search_t_id = tk.Label(self.teacher_mod_frame, text='---', font=config.ENTRY_FONT)
+        self.label_teacher_mod_chosen_year = tk.Label(self.teacher_mod_frame, text='', font=config.ENTRY_FONT)
+        self.entry_teacher_mod_score = tk.Entry(self.teacher_mod_frame, textvariable=self.tm_mod_score,
+                                                font=config.ENTRY_FONT, width=config.CD_MOD_ADD_ENTRY_WIDTH[5])
 
+        # 4
+        self.label_teacher_mod_chosen_year_hint = tk.Label(self.teacher_mod_frame, text='Chosen Year',
+                                                           font=config.LABEL_FONT)
+        self.label_teacher_mod_score_hint = tk.Label(self.teacher_mod_frame, text='Score', font=config.LABEL_FONT)
 
+        # 5
+        self.label_teacher_update_succeed_status = tk.Label(self.teacher_mod_frame, text='',
+                                                            font=config.EXCEPTION_FONT, fg='red')
 
+        # 1
+        self.label_teacher_mod_search_s_id_hint.grid(row=0, column=0)
+        self.entry_teacher_mod_search_s_id.grid(row=1, column=0)
+        self.label_teacher_mod_search_c_id_hint.grid(row=0, column=1)
+        self.entry_teacher_mod_search_c_id.grid(row=1, column=1)
+        self.label_teacher_mod_search_t_id_hint.grid(row=0, column=2)
+        self.entry_teacher_mod_search_t_id.grid(row=1, column=2)
+        # 2
+        self.button_tm_id_search.grid(row=1, column=3, pady=13)
+        self.button_tm_update.grid(row=1, column=4)
 
-
-
-
+        # 3
+        self.label_teacher_mod_chosen_year_hint.grid(row=2, column=3)
+        self.label_teacher_mod_score_hint.grid(row=2, column=4)
+        # 4
+        self.label_teacher_mod_search_s_id.grid(row=3, column=0)
+        self.label_teacher_mod_search_c_id.grid(row=3, column=1)
+        self.label_teacher_mod_search_t_id.grid(row=3, column=2)
+        self.label_teacher_mod_chosen_year.grid(row=3, column=3)
+        self.entry_teacher_mod_score.grid(row=3, column=4)
+        # 5
+        self.label_teacher_update_succeed_status.grid(row=4, column=2)
 
         # Create Add Frame
         self.add_frame = tk.Frame(self.main_frame)
@@ -258,9 +290,6 @@ class ChooseDetailView:
         # 4
         self.label_add_success_status.grid(row=2, column=2)
 
-
-
-
         # Pack Each Frame into Main Frame
         self.entry_frame.pack(pady=2)
         self.tree_frame.pack()
@@ -288,11 +317,15 @@ class ChooseDetailView:
     def login(self, status):
         if status[0] == 'Admin':
             self.mod_frame.pack(pady=5)
+        elif status[0] == 'Teacher':
+            self.teacher_mod_frame.pack(pady=5)
+
 
     def logout(self):
         self.reset_all_tracking_var()
         self.add_frame.pack_forget()
         self.mod_frame.pack_forget()
+        self.teacher_mod_frame.pack_forget()
 
     # Search/ID Search/Add/Delete/Update
     def search(self):
@@ -302,7 +335,6 @@ class ChooseDetailView:
         c_name = self.c_name.get().title()
         t_id = self.t_id.get()
         t_name = self.t_name.get().title()
-
 
         with sqlite3.connect(database='Student Info.db') as db:
             has_where = False
@@ -430,7 +462,6 @@ VALUES ('''
                 else:
                     SQL += "null, "
 
-
                 SQL += "null)"
 
                 print(SQL)
@@ -484,6 +515,67 @@ SET'''
                     SQL += '''    "Chosen Year" = null
 '''
 
+                SQL += '''WHERE "Student ID" =  %s AND "Course ID" =  %s And "Teacher ID" =  %s ''' % (s_id, c_id, t_id)
+
+                print(SQL)
+                temp_cursor.execute(SQL)
+                temp_cursor.close()
+        except sqlite3.Error:
+            self.label_update_succeed_status.config(text='Error: illegal format')
+            succeed = False
+
+        if succeed:
+            self.label_update_succeed_status.config(text='')
+            self.search()
+
+    def tm_id_search(self):
+        s_id = self.teacher_mod_search_s_id.get()
+        c_id = self.teacher_mod_search_c_id.get()
+        t_id = self.teacher_mod_search_t_id.get()
+        with sqlite3.connect(database='Student Info.db') as db:
+            temp_cursor = db.cursor()
+            SQL = '''SELECT * From Choose 
+WHERE "Student ID" = %s AND "Course ID" = %s AND "Teacher ID" = %s ''' % (s_id, c_id, t_id)
+
+            print(SQL)
+            temp_cursor.execute(SQL)
+            temp_result = temp_cursor.fetchall()
+            temp_cursor.close()
+            if temp_result:
+                self.tm_current_selected_s_id = temp_result[0][0]
+                self.label_teacher_mod_search_s_id.configure(text=temp_result[0][0])
+                self.tm_current_selected_c_id = temp_result[0][1]
+                self.label_teacher_mod_search_c_id.config(text=temp_result[0][1])
+                self.tm_current_selected_t_id = temp_result[0][2]
+                self.label_teacher_mod_search_t_id.config(text=temp_result[0][2])
+                self.label_teacher_mod_chosen_year.config(text=temp_result[0][3])
+                self.tm_mod_score.set(temp_result[0][4])
+            else:
+                self.tm_set_id_search_result()
+
+    def tm_update(self):
+        s_id = self.tm_current_selected_s_id
+        c_id = self.tm_current_selected_c_id
+        t_id = self.tm_current_selected_t_id
+        if s_id == '---' or c_id == "---" or t_id == '---':
+            return
+        score = self.tm_mod_score.get()
+
+        succeed = True
+        try:
+            with sqlite3.connect(database='Student Info.db') as db:
+                temp_cursor = db.cursor()
+                SQL = '''PRAGMA foreign_keys = ON;'''
+                temp_cursor.execute(SQL)
+
+                SQL = '''UPDATE Choose 
+SET'''
+                if score:
+                    SQL += '''    "Score" = '%s'
+''' % score
+                else:
+                    SQL += '''    "Score" = null
+'''
 
                 SQL += '''WHERE "Student ID" =  %s AND "Course ID" =  %s And "Teacher ID" =  %s ''' % (s_id, c_id, t_id)
 
@@ -508,6 +600,17 @@ SET'''
         self.label_mod_search_t_id.config(text=_t_id)
         self.mod_chosen_year.set(_chosen_year)
         self.label_mod_score.config(text=_score)
+
+    def tm_set_id_search_result(self, _s_id='---', _c_id='---', _t_id='---',
+                                _chosen_year='', _score=''):
+        self.tm_current_selected_s_id = _s_id
+        self.label_teacher_mod_search_s_id.configure(text=_s_id)
+        self.tm_current_selected_c_id = _c_id
+        self.label_teacher_mod_search_c_id.config(text=_c_id)
+        self.tm_current_selected_t_id = _t_id
+        self.label_teacher_mod_search_t_id.config(text=_t_id)
+        self.label_teacher_mod_chosen_year.config(text=_chosen_year)
+        self.tm_mod_score.set(_score)
 
     # Toggle Add Frame
     def toggle_add(self):
@@ -536,6 +639,7 @@ SET'''
         self.add_expanded = False
         self.clear_tree()
 
+        # 1
         self.s_id.set('')
         self.s_name.set('')
         self.c_id.set('')
@@ -543,15 +647,23 @@ SET'''
         self.t_id.set('')
         self.t_name.set('')
 
+        # 2
         self.mod_search_s_id.set('')
         self.mod_search_c_id.set('')
         self.mod_search_t_id.set('')
         self.set_id_search_result()
 
+        # 3
         self.add_s_id.set('')
         self.add_c_id.set('')
         self.add_t_id.set('')
         self.add_chosen_year.set('')
+
+        # 4
+        self.teacher_mod_search_s_id.set('')
+        self.teacher_mod_search_c_id.set('')
+        self.teacher_mod_search_t_id.set('')
+        self.tm_set_id_search_result()
 
         self.reset_update_success_status('', '', '')
         self.reset_add_success_status('', '', '')
